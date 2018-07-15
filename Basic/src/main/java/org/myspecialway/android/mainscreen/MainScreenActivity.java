47ae99f5a9dbc1272e22f0c268036a021d264082
Buchshreiber, Tal(tb399d) <tb399d@intl.att.com>
2018-07-15 14:20:03 +0300

    Complete layout implementation. Connect to layout xml Implement onClickListener for scheduleNavButton Load Avatar from URL

--- Basic/src/main/java/org/myspecialway/android/mainscreen/MainScreenActivity.java
+++ Basic/src/main/java/org/myspecialway/android/mainscreen/MainScreenActivity.java
@@ -1,29 +1,49 @@
-package org.myspecialway.android;
+package org.myspecialway.android.mainscreen;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
-import android.graphics.drawable.Drawable;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import org.myspecialway.android.R;
+
 public class MainScreenActivity extends AppCompatActivity {
-    TextView userNameView = (TextView)findViewById(R.id.user_display_name);
-    ImageView userAvatarView = (ImageView) findViewById(R.id.user_avatar_image);
-    TextView currentScheduleNameView = (TextView)findViewById(R.id.current_schedule_name_text);
+    TextView userNameView;
+    ImageView userAvatarView;
+    TextView currentScheduleNameView;
+    Button scheduleNavButton;
     MainScreenViewModel viewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+//        ViewDataBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main_screen);
         viewModel = ViewModelProviders.of(this).get(MainScreenViewModel.class);
+
+
+        userNameView = findViewById(R.id.user_display_name);
+        userAvatarView = findViewById(R.id.user_avatar_image);
+        currentScheduleNameView =findViewById(R.id.current_schedule_name_text);
+        scheduleNavButton = findViewById(R.id.schedule_button);
+
         observeUserName();
         observeUserAvatar();
         observeCurrentScheduleName();
-        viewModel.getUserName().setValue("BuchBuch");
+        scheduleNavButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainScreenActivity.this,MainScreenActivity.class));
+            }
+        });
     }
 
     private void observeUserName() {
@@ -34,13 +54,16 @@ public class MainScreenActivity extends AppCompatActivity {
             }
         };
         viewModel.getUserName().observe(this, userNameObserver);
+
+
     }
 
     private void observeUserAvatar() {
-        final Observer<Drawable> userAvatarObserver = new Observer<Drawable>() {
+        final Observer<String> userAvatarObserver = new Observer<String>() {
             @Override
-            public void onChanged(@Nullable final Drawable userAvatar) {
-                userAvatarView.setImageDrawable(userAvatar);
+            public void onChanged(@Nullable final String userAvatar) {
+
+                Glide.with(MainScreenActivity.this).load(userAvatar).into(userAvatarView);
             }
         };
         viewModel.getUserAvatar().observe(this, userAvatarObserver);
@@ -55,6 +78,7 @@ public class MainScreenActivity extends AppCompatActivity {
         };
         viewModel.getCurrentSheduleName().observe(this, currentScheduleName);
     }
+
 
 
 }

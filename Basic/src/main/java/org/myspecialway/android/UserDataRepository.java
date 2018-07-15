6b4374a09350eab35c27c69df84e1fb80167c1ac
Buchshreiber, Tal(tb399d) <tb399d@intl.att.com>
2018-07-15 14:22:07 +0300

    Add UserDataRepository object

---
+++ Basic/src/main/java/org/myspecialway/android/UserDataRepository.java
@@ -0,0 +1,10 @@
+package org.myspecialway.android;
+
+public class UserDataRepository {
+
+    UserData userData = new UserData("User1");
+
+    public UserData getUserData() {
+        return userData;
+    }
+}

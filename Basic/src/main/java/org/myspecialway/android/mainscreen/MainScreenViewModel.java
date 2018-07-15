dd3277d0c4d0a8fffb0339ebd5f851dbe2c31acd
Buchshreiber, Tal(tb399d) <tb399d@intl.att.com>
2018-07-15 14:21:04 +0300

    Add MainScreenViewModel

---
+++ Basic/src/main/java/org/myspecialway/android/mainscreen/MainScreenViewModel.java
@@ -0,0 +1,39 @@
+package org.myspecialway.android.mainscreen;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+
+import org.myspecialway.android.ScheduleRepository;
+import org.myspecialway.android.UserDataRepository;
+
+public class MainScreenViewModel extends ViewModel {
+
+    UserDataRepository userDataRepository;
+    ScheduleRepository scheduleRepository;
+
+    public MainScreenViewModel(UserDataRepository userDataRepository, ScheduleRepository scheduleRepository) {
+        this.userDataRepository = userDataRepository;
+        this.scheduleRepository = scheduleRepository;
+    }
+
+    public LiveData<String> getUserName() {
+
+         return userDataRepository.getUserData().getUserName();
+    }
+
+
+    public LiveData<String> getUserAvatar() {
+
+        return userDataRepository.getUserData().getUserAvatar();
+    }
+
+
+
+    public LiveData<String> getCurrentSheduleName() {
+
+        return scheduleRepository.getScheduleData().getCurrentScheduleName();
+    }
+
+
+
+}

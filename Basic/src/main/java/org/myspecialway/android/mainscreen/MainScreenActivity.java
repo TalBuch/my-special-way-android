6e37ff189a65c453de465867727e0d5b63520e86
Buchshreiber, Tal(tb399d) <tb399d@intl.att.com>
2018-07-19 13:07:21 +0300

    Add imports

--- Basic/src/main/java/org/myspecialway/android/mainscreen/MainScreenActivity.java
+++ Basic/src/main/java/org/myspecialway/android/mainscreen/MainScreenActivity.java
@@ -3,17 +3,21 @@ package org.myspecialway.android.mainscreen;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.bumptech.glide.Glide;
+
 import org.myspecialway.AgendaActivity;
 import org.myspecialway.android.ListExamplesActivity;
+import org.myspecialway.android.R;
 import org.myspecialway.android.ScheduleRepository;
 import org.myspecialway.android.UserDataRepository;
 
@@ -29,7 +33,7 @@ public class MainScreenActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-//        ViewDataBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main_screen);
+        ViewDataBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main_screen);
         viewModel = ViewModelProviders.of(this).get(MainScreenViewModel.class);
         viewModel.setRepos(new UserDataRepository(), new ScheduleRepository());
 
@@ -91,7 +95,7 @@ public class MainScreenActivity extends AppCompatActivity {
         };
         viewModel.getCurrentScheduleName().observe(this, currentScheduleName);
     }
-
-
-
+//
+//
+//
 }

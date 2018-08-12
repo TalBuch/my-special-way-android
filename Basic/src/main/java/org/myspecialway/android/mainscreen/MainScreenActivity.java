9c8730713e78896e300a4855eb7bbc1a1d4663ed
Buchshreiber, Tal(tb399d) <tb399d@intl.att.com>
2018-08-13 00:33:51 +0300

    Merge with maser

--- Basic/src/main/java/org/myspecialway/android/mainscreen/MainScreenActivity.java
+++ Basic/src/main/java/org/myspecialway/android/mainscreen/MainScreenActivity.java
@@ -5,14 +5,12 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.myspecialway.AgendaActivity;
-import org.myspecialway.android.ListExamplesActivity;
 import org.myspecialway.android.MswApplication;
 import org.myspecialway.android.R;
 import org.myspecialway.android.schedule.ScheduleRepository;
